ca9baf4520419575fd7af7476b26dff7cf7aee31
Michael Klear <michael.klear@nike.com>
2019-10-17 21:26:20 +0000

    update setup.py for v0.0.3

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ requirements = [
     'pyspark'
 ]
 
-#public version 0.0.2
-version = '0.0.2'
+#public version 0.0.3
+version = '0.0.3'
 
 setup(
     name='search-tools',
     version=version,
     author="Michael Klear",
     author_email='fixityourself@fu.com',
-    url='https://github.com/AlliedToasters/search-tools/archive/v0.0.1.tar.gz',
+    url='https://github.com/AlliedToasters/search-tools/archive/v0.0.3.tar.gz',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements
