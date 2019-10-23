de7700058820ffc38d98f5e8355ef641d649c6c1
Michael Klear <michael.klear@nike.com>
2019-10-22 20:37:40 -0700

    update setup.py for 0.0.4; add scipy as dependency

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@ import sys
 
 requirements = [
     'numpy',
+    'scipy',
     'pandas',
     'pyspark'
 ]
 
-#public version 0.0.3
-version = '0.0.3'
+#public version 0.0.4
+version = '0.0.4'
 
 setup(
     name='search-tools',
     version=version,
     author="Michael Klear",
     author_email='fixityourself@fu.com',
-    url='https://github.com/AlliedToasters/search-tools/archive/v0.0.3.tar.gz',
+    url='https://github.com/AlliedToasters/search-tools/archive/v0.0.4.tar.gz',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements
